73d1a6e6db8cefa6bc2ff3807baae079f7bc55e5
inTheMembrane <naughtnaught@outlook.com>
2023-07-31 17:43:35 +0200

    FEAT add reminder date option to tasks

--- src/components/taskElement.tsx
+++ src/components/taskElement.tsx
@@ -6,7 +6,7 @@ import { Task } from '../customTypes'
 
 export default function TaskElement({
   task,
-  reloadList,
+  reloadList
 }: {
   task: Task
   reloadList: () => void
@@ -17,6 +17,7 @@ export default function TaskElement({
         id
         name
         active
+        reminderDate
       }
     }
   `
@@ -26,17 +27,56 @@ export default function TaskElement({
     }
   `
 
-  const { id, name, active } = task
+  const { id, name, active, reminderDate } = task
 
   const [taskUpdate] = useMutation(UPDATE_TASK)
 
   const [taskDelete] = useMutation(DELETE_TASK)
 
+  // Checks if task should be reminded
+  function isTimeToRemind(date: any) {
+    // console.log('typeof date : ', typeof date)
+    // FIXME (PPDS) : reminderDate received is in string format but expected number
+    if (Date.parse(date) <= new Date().setHours(0, 0, 0, 0)) {
+      return true
+    } else return false
+  }
+
+  // Toggles a reminder by changing date to null if existing or changing it to current date
+  async function handleToggleReminder(e: MouseEvent<HTMLButtonElement>) {
+    const todayDate = new Date().setHours(0, 0, 0, 0)
+    await taskUpdate({
+      variables: {
+        task: {
+          id: id,
+          reminderDate: reminderDate ? null : todayDate
+        }
+      }
+    })
+    reloadList()
+  }
+
+  async function handleUpdateReminder(
+    e: ChangeEvent<HTMLInputElement>
+  ) {
+    const newDate = Date.parse(e.currentTarget.value)
+
+    await taskUpdate({
+      variables: {
+        task: {
+          id: id,
+          reminderDate: newDate
+        }
+      }
+    })
+    reloadList()
+  }
+
   async function handleCheck(e: ChangeEvent<HTMLInputElement>) {
     await taskUpdate({
       variables: {
-        task: { id: id, active: !e.currentTarget.checked },
-      },
+        task: { id: id, active: !e.currentTarget.checked }
+      }
     })
     reloadList()
   }
@@ -44,17 +84,17 @@ export default function TaskElement({
   function handleDelete(e: MouseEvent<HTMLButtonElement>) {
     taskDelete({
       variables: {
-        id: id,
-      },
+        id: id
+      }
     })
   }
-
+  
   const { callback: debouncedHandleUpdate }: any = useDebouncedCallback(
     (value: any) => {
       taskUpdate({
         variables: {
-          task: { id: id, name: value },
-        },
+          task: { id: id, name: value }
+        }
       })
     },
     1000
@@ -76,9 +116,24 @@ export default function TaskElement({
       </div>
       <input
         className={`todo-text ${!active && 'todo-checked-text'}`}
-        onChange={(e) => debouncedHandleUpdate(e.currentTarget.value)}
+        onChange={e => debouncedHandleUpdate(e.currentTarget.value)}
         defaultValue={name}
       ></input>
+
+      {/* Date input that is hidden if reminder date not defined + highlighted if date current or past via CSS */}
+      <input
+        type="date"
+        className={`todo-reminder${!reminderDate ? '-hidden' : ''} ${isTimeToRemind(
+          reminderDate
+        ) && 'todo-reminder-current'}`}
+        onChange={handleUpdateReminder}
+        value={new Date(reminderDate).toISOString().split('T')[0]}
+      ></input>
+      
+       {/* Button icon that toggles date selection by adding or removing value for reminderDate */}
+      <button className="set-reminder-button" onClick={handleToggleReminder}>
+        {reminderDate ? <span>&#128277;</span> : <span>&#128276;</span>}
+      </button>
       <button className="delete-button" onClick={handleDelete}>
         ×
       </button>
